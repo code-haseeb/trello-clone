5c8ca4078450500a550de45f1ac4320fd9f85126
code-haseeb <abdullhaseebonline@gmail.com>
2024-02-17 22:19:39 +0500

    global body styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,25 @@
 
 import LoginView from "@/components/views/LoginView";
-import {authOptions} from "@/lib/authOptions";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {getServerSession} from "next-auth";
+import { authOptions } from "@/lib/authOptions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
-import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 export default async function Home() {
   const session = await getServerSession(authOptions);
   if (!session) {
     return (
       <LoginView />
     );
-  } 
+  }
   return (
-  <div>
+    <div>
       <h1 className="text-4xl mb-4">Your boards</h1>
-        <div className="mt-4">
+      <div className="mt-4">
         <Link
           className="btn primary inline-flex gap-2"
           href={'/new-board'}>
-          Create new board <FontAwesomeIcon className="h-6" icon={faArrowRight}/>
+          Create new board <FontAwesomeIcon className="h-6" icon={faArrowRight} />
         </Link>
       </div>
     </div>
